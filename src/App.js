4d2156b157c56e86fa0ace2ba0658a0d762523cd
Chau Dinh <chaudvb95uc@gmail.com>
2020-04-05 16:56:26 +0700

    refactor ui

--- src/App.js
+++ src/App.js
@@ -3,44 +3,42 @@ import React from "react";
 import "./App.css";
 import Form from "./components/Form/Form";
 import Countdown from "./components/Countdown/Countdown";
-import Logo from "./assets/images/tiki_logo.png";
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header>
-          <img src={Logo} alt="logo" />
-        </header>
+	render() {
+		return (
+			<div className="App">
+				<main>
+					<h1>Countdown React.js</h1>
 
-        <main>
-          <h1>The time you start working at Tiki</h1>
+					<Form />
+					<Countdown />
+				</main>
 
-          <Form />
-          <Countdown />
-        </main>
-
-        <footer>
-          <p>
-            Created by{" "}
-            <span>
-              Chau Dinh{" "}
-              <span aria-label="shit" role="img">
-                💩
-              </span>{" "}
-              2020
-            </span>
-          </p>
-          <a href="https://github.com/ChauDinh">
-            <i className="fab fa-github"></i>
-          </a>
-          <a href="https://linkedin.com">
-            <i className="fab fa-linkedin"></i>
-          </a>
-        </footer>
-      </div>
-    );
-  }
+				<footer>
+					<p>
+						Created by{" "}
+						<span>
+							Chau Dinh{" "}
+							<span
+								aria-label="shit"
+								role="img"
+							>
+								❤️
+							</span>{" "}
+							2020
+						</span>
+					</p>
+					<a href="https://github.com/ChauDinh">
+						<i className="fab fa-github"></i>
+					</a>
+					<a href="https://linkedin.com">
+						<i className="fab fa-linkedin"></i>
+					</a>
+				</footer>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Countdown/Countdown.js
+++ src/components/Countdown/Countdown.js
@@ -1,174 +1,260 @@
 import React from "react";
 
 import "./Countdown.css";
+import { Alert } from "../Alert/Alert.jsx";
 
 const initialState = {
-  days: 0,
-  months: 0,
-  years: 0,
-  minutes: 0,
-  seconds: 0,
-  hours: 0,
-  timeLeft: 0
+	days: 0,
+	months: 0,
+	years: 0,
+	minutes: 0,
+	seconds: 0,
+	hours: 0,
+	timeLeft: 0
 };
 
 export default class Countdown extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.getTime = this.getTime.bind(this);
-    this.fixLeadingZero = this.fixLeadingZero.bind(this);
-  }
+		this.getTime = this.getTime.bind(this);
+		this.fixLeadingZero = this.fixLeadingZero.bind(this);
+	}
 
-  state = initialState;
+	state = initialState;
 
-  componentDidMount() {
-    this.getTime();
-    if (this.state.seconds === 0) {
-      // alert("Countdown Completed!");
-      clearInterval(this.timerID);
-    }
-    this.timerID = setInterval(() => {
-      this.getTime();
-    }, 10);
-  }
+	componentDidMount() {
+		this.getTime();
+		if (this.state.seconds === 0) {
+			// alert("Countdown Completed!");
+			clearInterval(this.timerID);
+		}
+		this.timerID = setInterval(() => {
+			this.getTime();
+		}, 10);
+	}
 
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
 
-  shouldComponentUpdate() {
-    if (this.state.seconds < 0) {
-      return false;
-    }
-    return true;
-  }
+	shouldComponentUpdate() {
+		if (this.state.seconds < 0) {
+			return false;
+		}
+		return true;
+	}
 
-  getTime() {
-    const timeLeft =
-      localStorage.getItem("date.parse") - Date.parse(new Date());
-    const seconds = Math.floor((timeLeft / 1000) % 60);
-    const minutes = Math.floor((timeLeft / 1000 / 60) % 60);
-    const hours = Math.floor((timeLeft / 1000 / 60 / 60) % 24);
-    const days = Math.floor((timeLeft / 1000 / 60 / 60 / 24) % 30);
-    const months = Math.floor((timeLeft / 1000 / 60 / 60 / 24 / 30) % 12);
-    const years = Math.floor(timeLeft / 1000 / 60 / 60 / 24 / 365);
+	getTime() {
+		const timeLeft =
+			localStorage.getItem("date.parse") -
+			Date.parse(new Date());
+		const seconds = Math.floor((timeLeft / 1000) % 60);
+		const minutes = Math.floor((timeLeft / 1000 / 60) % 60);
+		const hours = Math.floor((timeLeft / 1000 / 60 / 60) % 24);
+		const days = Math.floor((timeLeft / 1000 / 60 / 60 / 24) % 30);
+		const months = Math.floor(
+			(timeLeft / 1000 / 60 / 60 / 24 / 30) % 12
+		);
+		const years = Math.floor(timeLeft / 1000 / 60 / 60 / 24 / 365);
 
-    this.setState({ days, hours, minutes, seconds, years, months, timeLeft });
-  }
+		this.setState({
+			days,
+			hours,
+			minutes,
+			seconds,
+			years,
+			months,
+			timeLeft
+		});
+	}
 
-  fixLeadingZero(num) {
-    return num >= 0 && num < 10 ? "0" + num : num;
-  }
+	fixLeadingZero(num) {
+		return num >= 0 && num < 10 ? "0" + num : num;
+	}
 
-  render() {
-    return (
-      <div className="countdown__wrapper">
-        {this.state.timeLeft < 0 ? (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              fontSize: "18px"
-            }}
-          >
-            <i
-              className="far fa-check-circle"
-              style={{
-                color: "#00ff00",
-                fontSize: "40px",
-                marginBottom: "10px"
-              }}
-            ></i>{" "}
-            Countdown Completed!
-          </div>
-        ) : (
-          <>
-            <div className="years">
-              <span className="value">
-                {this.fixLeadingZero(
-                  this.state.years < 0 ? 0 : this.state.years
-                )}
-              </span>
-              Years
-            </div>
-            <div
-              className="colon"
-              style={{
-                fontSize: "30px",
-                margin: "0 10px",
-                color: "#fff"
-              }}
-            >
-              :
-            </div>
-            <div className="months">
-              <span className="value">
-                {this.fixLeadingZero(
-                  this.state.months < 0 ? 0 : this.state.months
-                )}
-              </span>
-              Months
-            </div>
-            <div
-              className="colon"
-              style={{ fontSize: "30px", margin: "0 10px", color: "#fff" }}
-            >
-              :
-            </div>
-            <div className="days">
-              <span className="value">
-                {this.fixLeadingZero(this.state.days < 0 ? 0 : this.state.days)}
-              </span>
-              Days
-            </div>
-            <div
-              className="colon"
-              style={{ fontSize: "30px", margin: "0 10px", color: "#fff" }}
-            >
-              :
-            </div>
-            <div className="hours">
-              <span className="value">
-                {this.fixLeadingZero(
-                  this.state.hours < 0 ? 0 : this.state.hours
-                )}
-              </span>
-              Hours
-            </div>
-            <div
-              className="colon"
-              style={{ fontSize: "30px", margin: "0 10px", color: "#fff" }}
-            >
-              :
-            </div>
-            <div className="minutes">
-              <span className="value">
-                {this.fixLeadingZero(
-                  this.state.minutes < 0 ? 0 : this.state.minutes
-                )}
-              </span>
-              Minutes
-            </div>
-            <div
-              className="colon"
-              style={{ fontSize: "30px", margin: "0 10px", color: "#fff" }}
-            >
-              :
-            </div>
-            <div className="seconds">
-              <span className="value">
-                {this.fixLeadingZero(
-                  this.state.seconds < 0 ? 0 : this.state.seconds
-                )}
-              </span>
-              Seconds
-            </div>
-          </>
-        )}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="countdown__wrapper">
+				{this.state.timeLeft < 0 ? (
+					<div
+						style={{
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "center",
+							justifyContent:
+								"center",
+							fontSize: "18px"
+						}}
+					>
+						<i
+							className="far fa-check-circle"
+							style={{
+								color:
+									"#00ff00",
+								fontSize:
+									"40px",
+								marginBottom:
+									"10px"
+							}}
+						></i>{" "}
+						Countdown Completed!
+					</div>
+				) : (
+					<>
+						<div className="years">
+							<span className="value">
+								{this.fixLeadingZero(
+									this
+										.state
+										.years <
+										0
+										? 0
+										: this
+												.state
+												.years
+								)}
+							</span>
+							Years
+						</div>
+						<div
+							className="colon"
+							style={{
+								fontSize:
+									"30px",
+								margin:
+									"0 10px",
+								color: "#fff"
+							}}
+						>
+							:
+						</div>
+						<div className="months">
+							<span className="value">
+								{this.fixLeadingZero(
+									this
+										.state
+										.months <
+										0
+										? 0
+										: this
+												.state
+												.months
+								)}
+							</span>
+							Months
+						</div>
+						<div
+							className="colon"
+							style={{
+								fontSize:
+									"30px",
+								margin:
+									"0 10px",
+								color: "#fff"
+							}}
+						>
+							:
+						</div>
+						<div className="days">
+							<span className="value">
+								{this.fixLeadingZero(
+									this
+										.state
+										.days <
+										0
+										? 0
+										: this
+												.state
+												.days
+								)}
+							</span>
+							Days
+						</div>
+						<div
+							className="colon"
+							style={{
+								fontSize:
+									"30px",
+								margin:
+									"0 10px",
+								color: "#fff"
+							}}
+						>
+							:
+						</div>
+						<div className="hours">
+							<span className="value">
+								{this.fixLeadingZero(
+									this
+										.state
+										.hours <
+										0
+										? 0
+										: this
+												.state
+												.hours
+								)}
+							</span>
+							Hours
+						</div>
+						<div
+							className="colon"
+							style={{
+								fontSize:
+									"30px",
+								margin:
+									"0 10px",
+								color: "#fff"
+							}}
+						>
+							:
+						</div>
+						<div className="minutes">
+							<span className="value">
+								{this.fixLeadingZero(
+									this
+										.state
+										.minutes <
+										0
+										? 0
+										: this
+												.state
+												.minutes
+								)}
+							</span>
+							Minutes
+						</div>
+						<div
+							className="colon"
+							style={{
+								fontSize:
+									"30px",
+								margin:
+									"0 10px",
+								color: "#fff"
+							}}
+						>
+							:
+						</div>
+						<div className="seconds">
+							<span className="value">
+								{this.fixLeadingZero(
+									this
+										.state
+										.seconds <
+										0
+										? 0
+										: this
+												.state
+												.seconds
+								)}
+							</span>
+							Seconds
+						</div>
+					</>
+				)}
+			</div>
+		);
+	}
 }
